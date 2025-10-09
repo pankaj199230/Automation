d2a7d254623322bd16dbc13475dc6f59df2a64a1
pankaj.kamble <pankaj.kamble@indiabonds.com>
2025-10-09 16:31:43 +0530

    changes in sample file and new file sdet1 added

---
+++ src/testing/sdet1.java
@@ -0,0 +1,10 @@
+package testing;
+
+public class sdet1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("new file added by sdet1");
+	}
+
+}
