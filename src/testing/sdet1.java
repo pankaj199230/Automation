4a230d35c77651092ea317f9410612ab401798e9
pankaj.kamble <pankaj.kamble@indiabonds.com>
2025-10-09 20:27:37 +0530

    added code by pankaj

--- src/testing/sdet1.java
+++ src/testing/sdet1.java
@@ -5,6 +5,9 @@ public class sdet1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("new file added by pankaj kamble");
+		System.out.println("code added by pankaj 8:26 ");
+		System.out.println("code added by pankaj 8:27 ");
+		System.out.println("code added by pankaj 8:28 ");
 	}
 
 }
