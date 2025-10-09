25110554755df29694ae2e11a559a6f06db727c8
pankaj.kamble <pankaj.kamble@indiabonds.com>
2025-10-09 20:42:28 +0530

    adsdsadadd

--- src/testing/sdet1.java
+++ src/testing/sdet1.java
@@ -7,13 +7,14 @@ public class sdet1 {
 
 		System.out.println("new file added by swapnali");
 		System.out.println("new file added by sg");
-		System.out.println("new file added by pk");
+		System.out.println("new file added by pk ho kya");
 		System.out.println("new file added by swapnil");
 		System.out.println("new file added by pankaj kamble");
 		System.out.println("code added by pankaj 8:26 ");
 		System.out.println("code added by pankaj 8:27 ");
 		System.out.println("code added by pankaj 8:28 ");
 		System.out.println("new line added 8:34 by sdet1");
+		System.out.println("new code added by pankaj 8:42");
 
 	}
 
