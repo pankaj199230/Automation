60335aee967c157004d1d31bc632ee261bbe551b
pankaj.kamble <pankaj.kamble@indiabonds.com>
2025-10-09 20:17:17 +0530

    test

--- src/testing/sdet1.java
+++ src/testing/sdet1.java
@@ -4,7 +4,7 @@ public class sdet1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("new file added by sdet1");
+		System.out.println("new file added by swapnil");
 	}
 
 }
