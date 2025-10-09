6db5c96929fb665942d6f376d547674a089d61d2
pankaj.kamble <pankaj.kamble@indiabonds.com>
2025-10-09 19:45:09 +0530

    changes by pankaj

--- src/testing/sdet2.java
+++ src/testing/sdet2.java
@@ -4,7 +4,7 @@ public class sdet2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("correction in sdet2");
+		System.out.println("correction in sdet2 file");
 	}
 
 }
