22c40dec0d3ec3270830188d9b1d3f513b457850
pankaj.kamble <pankaj.kamble@indiabonds.com>
2025-10-09 19:43:18 +0530

    changes added by superuser

--- src/testing/sdet2.java
+++ src/testing/sdet2.java
@@ -4,7 +4,7 @@ public class sdet2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("correction in sdet2");
+		System.out.println("1new code added by superuser");
 	}
 
 }
