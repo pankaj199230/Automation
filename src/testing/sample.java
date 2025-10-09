edd8d4b34bc5965560f4b416fd535cf711f72064
pankaj.kamble <pankaj.kamble@indiabonds.com>
2025-10-09 14:11:01 +0530

    superuser added the changes

---
+++ src/testing/sample.java
@@ -0,0 +1,11 @@
+package testing;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("code added by superuser");
+		System.out.println("code added by superuser");
+	}
+
+}
