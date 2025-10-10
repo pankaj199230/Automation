a8f1be314a1c1ae7c06154d70f84854b6a9cd9df
pankaj.kamble <pankaj.kamble@indiabonds.com>
2025-10-10 19:06:52 +0530

    new file added

---
+++ src/testing/example.java
@@ -0,0 +1,14 @@
+package testing;
+
+public class example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("line1");
+		System.out.println("line2");
+		System.out.println("line3");
+		
+	}
+
+}
