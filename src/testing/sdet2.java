9a0e9de94a0b83dcdfbdae9c11c3e2f04af80020
pankaj.kamble <pankaj.kamble@indiabonds.com>
2025-10-10 19:19:31 +0530

    tesr

--- src/testing/sdet2.java
+++ src/testing/sdet2.java
@@ -13,7 +13,9 @@ public class sdet2 {
 		System.out.println("code added by sdet1");
 		System.out.println("new feature added");
 		System.out.println("new integration added");
-		System.out.println("new code added 6:52 pm");
+		System.out.println("new code removed 6:52 pm");
+		System.out.println("line added at 7:18");
+		
 		
 
 
