ba05c0d0065ab6df20868894f7ce92c570c208ae
pankaj.kamble <pankaj.kamble@indiabonds.com>
2025-10-10 19:10:08 +0530

    test

--- src/testing/sdet2.java
+++ src/testing/sdet2.java
@@ -6,19 +6,15 @@ public class sdet2 {
 		// TODO Auto-generated method stub
 		System.out.println("correction in sdet2 file");
 		System.out.println("1new code added by superuser");
-		System.out.println("lie added by sdet2 user 8:03 pm");
-<<<<<<< HEAD
-		
+		System.out.println("lie added by sdet2 user 8:03 pm");	
 		System.out.println("code added by pk 6:56");
 		System.out.println("sdet1 added the code 6:56");
 		System.out.println("sdet1 added the code 6:56");
-=======
 		System.out.println("code added by sdet1");
 		System.out.println("new feature added");
 		System.out.println("new integration added");
 		System.out.println("new code added 6:52 pm");
 		
->>>>>>> 1cb7b7b9baf6418de68a93ed5fd80ce16cd57f6a
 
 
 	}
